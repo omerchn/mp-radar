c07fdfc58c0a49b2371b361deeac78a589d0c795
Omer Cohen <omer.oref@gmail.com>
2022-11-07 14:08:18 +0100

    bug fix

--- client/src/pages/map_page/index.tsx
+++ client/src/pages/map_page/index.tsx
@@ -106,7 +106,6 @@ function ResetFollowUser({
 }) {
   useMapEvents({
     drag: () => followUser && setFollowUser(false),
-    zoom: (e) => followUser && setFollowUser(false),
   })
   return null
 }
